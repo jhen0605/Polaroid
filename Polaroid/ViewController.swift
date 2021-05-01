d9151243a0c521ee973143871a9f2a0582173ac1
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-05-02 02:17:46 +0800

    Initial Commit

---
+++ Polaroid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Polaroid
+//
+//  Created by 簡吟真 on 2021/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
